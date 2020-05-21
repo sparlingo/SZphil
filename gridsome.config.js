24fdb634374446bd4c3aa2bcc38e1b459d5b0420
Kevin Sparling <kevinsparling@gmail.com>
2020-05-21 19:10:36 +0800

    working on polls yo

--- gridsome.config.js
+++ gridsome.config.js
@@ -41,6 +41,16 @@ module.exports = {
       }
     },
     {
+      use: '@gridsome/source-contentful',
+      options: {
+        space: 'x3kgssebouhp',
+        accessToken: '-3UsOMJv4F1xYG-PqlE9PC25jpo2VeCvKw7sO15C1kg', // required
+        host: 'cdn.contentful.com',
+        environment: 'master',
+        typeName: 'Contentful'
+      }
+    },
+    {
       use: '@gridsome/plugin-sitemap',
       options: {
         cacheTime: 600000
