e03404b461e5b0669c977eb576f21b807cd1f07c
Kevin Sparling <kevinsparling@gmail.com>
2020-05-22 21:20:00 +0800

    added sanity plugin

---
+++ src/templates/Poll.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <h1>
+      {{ $page.doc.title }}
+    </h1>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.doc.title,
+      meta: [
+        { key: 'description', name: 'description', content: this.$page.doc.description }
+      ]
+    }
+  }
+}
+</script>
